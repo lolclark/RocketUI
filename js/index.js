950fe331207ebdcb2dc56396e534321a99bc31eb
Torquin <lol_clark@hotmail.com>
2026-02-19 00:58:40 +0100

    Update commands.json url

--- js/index.js
+++ js/index.js
@@ -9,8 +9,7 @@ let draggedElement = null;
 let historyIndex = -1;
 let selectedKeyPath = '';
 let currentAuthMode = 'password';
-// const GITHUB_URL = 'https://raw.githubusercontent.com/yourname/yourrepo/main/commands.json';
-const GITHUB_URL = './commands.json';
+const GITHUB_URL = 'https://raw.githubusercontent.com/lolclark/RocketUI/refs/heads/master/commands.json';
 
 /* =========================
 ② BOOTSTRAP FLOW
